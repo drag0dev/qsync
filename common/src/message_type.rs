85659a247f2e0649b8b98fcd547b64c1739e2f81
Dragoslav Tamindžija <dragodev@protonmail.com>
2025-04-14 14:24:17 +0200

    feat: introduce serialization for message types

--- common/src/message_type.rs
+++ common/src/message_type.rs
@@ -1,16 +1,25 @@
+use deku::{DekuRead, DekuWrite};
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(id_type = "u8")]
 pub enum MessageType {
     /// Client sends the dir/file information (paths, sizes, checksums)
+    #[deku(id = 0x01)]
     SyncRequest,
 
     /// Server responds to SyncRequest with its own dir/file information (paths, sizes, checksums)
+    #[deku(id = 0x02)]
     ChecksumsResponse,
 
     /// Client requests specific block by block index an path
+    #[deku(id = 0x03)]
     BlockRequest,
 
     /// Server responds to BlockRequest with block index, path, and the block data
+    #[deku(id = 0x04)]
     BlockData,
 
     /// Finalizes the transfer
+    #[deku(id = 0x05)]
     Complete
 }
