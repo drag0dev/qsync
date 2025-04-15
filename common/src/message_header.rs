aaeb0c9b5d4b046153143de48050876b3db8a32c
Dragoslav Tamindžija <dragodev@protonmail.com>
2025-04-15 14:17:42 +0200

    feat: switch to bincode

--- common/src/message_header.rs
+++ common/src/message_header.rs
@@ -1,7 +1,10 @@
 use super::message_type::MessageType;
-use deku::{DekuRead, DekuWrite};
+use serde::{Deserialize, Serialize};
+use anyhow::{Result, Context};
 
-#[derive(Debug, DekuRead, DekuWrite)]
+pub static HEADER_LEN: usize = 8 + 4;
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct MessageHeader {
     pub msg_len: u64,
     pub msg_type: MessageType,
@@ -13,5 +16,13 @@ impl MessageHeader {
             msg_type,
             msg_len,
         }
+    }
+
+    pub fn serialize(&self) -> Result<Vec<u8>> {
+        Ok(bincode::serialize(&self).context("serializing message header")?)
+    }
+
+    pub fn deserialize(header: &[u8]) -> Result<Self> {
+        Ok(bincode::deserialize(header).context("deserializing message header")?)
     }
 }
--- common/src/message_type.rs
+++ common/src/message_type.rs
@@ -1,25 +1,19 @@
-use deku::{DekuRead, DekuWrite};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
-#[deku(id_type = "u8")]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum MessageType {
     /// Client sends the dir/file information (paths, sizes, checksums)
-    #[deku(id = 0x01)]
     SyncRequest,
 
     /// Server responds to SyncRequest with its own dir/file information (paths, sizes, checksums)
-    #[deku(id = 0x02)]
     ChecksumsResponse,
 
     /// Client requests specific block by block index an path
-    #[deku(id = 0x03)]
     BlockRequest,
 
     /// Server responds to BlockRequest with block index, path, and the block data
-    #[deku(id = 0x04)]
     BlockData,
 
     /// Finalizes the transfer
-    #[deku(id = 0x05)]
     Complete
 }
