efb86f4039b2a9e0ef63d6aa0c6550a4c2ddd574
Dragoslav Tamindžija <dragodev@protonmail.com>
2025-04-16 15:01:58 +0200

    feat: file checksum iterator

---
+++ common/src/file_checksum_iter.rs
@@ -0,0 +1,34 @@
+use std::{
+    fs::File,
+    io::{Read, Seek}
+};
+use anyhow::{Context, Result};
+use super::CHUNK_SIZE;
+
+pub struct FileChecksumIter {
+    file: File,
+    buffer: [u8; CHUNK_SIZE],
+}
+
+impl FileChecksumIter {
+    pub fn new(path: &str) -> Result<Self> {
+        let mut file = File::open(path).context("opening file in checksum iter")?;
+        file.rewind().context("rewiding file in checksum iter")?;
+        Ok(FileChecksumIter { file, buffer: [0; CHUNK_SIZE] })
+    }
+}
+
+impl Iterator for FileChecksumIter {
+    type Item = Result<String>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.file.read(&mut self.buffer) {
+            Ok(0) => None,
+            Ok(n) => {
+                let hash = blake3::hash(&self.buffer[..n]);
+                Some(Ok(hash.to_string()))
+            }
+            Err (e) => Some(Err(e.into()))
+        }
+    }
+}
--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,4 +1,7 @@
 mod file_meta;
 pub mod message;
+mod file_checksum_iter;
 
 pub use file_meta::FileMeta;
+pub static CHUNK_SIZE: usize = 16 * 1024;
+pub use file_checksum_iter::FileChecksumIter;
