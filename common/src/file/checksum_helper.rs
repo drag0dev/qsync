9443ebf5386bdb085d93d2c9e4e1b0ce35aa3a44
Dragoslav Tamindžija <dragodev@protonmail.com>
2025-04-20 13:29:24 +0200

    feat: checksum helper

---
+++ common/src/file/checksum_helper.rs
@@ -0,0 +1,57 @@
+use std::{collections::VecDeque, path::Path};
+use anyhow::{anyhow, Context, Result};
+use super::{
+    FileMeta,
+    FileChecksumIter
+};
+
+pub fn get_checksums(path_str: &str) -> Result<Vec<FileMeta>> {
+    let path = Path::new(path_str);
+
+    // TODO: handle symlinks
+    let mut res = Vec::new();
+    if path.is_file() {
+        let checksums = get_file_checkums(&path_str).context("getting file checksums")?;
+        let meta = FileMeta::new(path_str.to_owned(), checksums);
+        res.push(meta);
+    }
+    else if path.is_symlink() {}
+    else {
+        let mut dirs = VecDeque::new();
+        dirs.push_back(path.to_owned());
+        while dirs.len() > 0 {
+            let mut n = dirs.len();
+            while n > 0 {
+                let dir_entries = dirs
+                    .pop_front()
+                    .unwrap()
+                    .read_dir()
+                    .context("reading directory items")?;
+                for entry in dir_entries {
+                    let entry = entry.context("reading entry in a directory")?;
+                    let entry = entry.path();
+
+                    let entry_item_path = entry.to_str();
+                    if entry_item_path.is_none() { return Err(anyhow!("Cannot get directory item path")); }
+                    let entry_item_path = entry_item_path.unwrap().to_owned();
+
+                    if entry.is_file() {
+                        let checksums = get_file_checkums(&entry_item_path).context("getting file checksums")?;
+                        let meta = FileMeta::new(entry_item_path, checksums);
+                        res.push(meta);
+                    } else if entry.is_symlink() {}
+                    else { dirs.push_back(entry); }
+                }
+                n -= 1;
+            }
+        }
+    }
+
+     Ok(res)
+}
+
+fn get_file_checkums(path: &str) -> Result<Vec<String>> {
+    let iter = FileChecksumIter::new(&path)
+        .context("instantiating file checksum iter")?;
+    iter.into_iter().collect()
+}
--- common/src/file/mod.rs
+++ common/src/file/mod.rs
@@ -1,6 +1,8 @@
 mod file_meta;
 mod file_checksum_iter;
+mod checksum_helper;
 
 pub use file_meta::FileMeta;
 pub use file_checksum_iter::FileChecksumIter;
 pub static CHUNK_SIZE: usize = 16 * 1024;
+pub use checksum_helper::get_checksums;
