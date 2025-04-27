9c0cacb414eb095705cd0fe20446b27e35e67173
Dragoslav Tamindžija <dragodev@protonmail.com>
2025-04-27 14:05:53 +0200

    fix: remove ser and deser function in file meta

--- common/src/file/file_meta.rs
+++ common/src/file/file_meta.rs
@@ -1,5 +1,4 @@
 use serde::{Deserialize, Serialize};
-use anyhow::{Result, Context};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct FileMeta {
@@ -15,13 +14,5 @@ pub struct FileMeta {
 impl FileMeta {
     pub fn new(path: String, checksums: Vec<String>, modified_timestamp: u128, size: u64) -> Self {
         FileMeta { path, checksums, modified_timestamp, size }
-    }
-
-    pub fn serialize(&self) -> Result<Vec<u8>> {
-        Ok(bincode::serialize(&self).context("serializing file meta")?)
-    }
-
-    pub fn deserialize(msg: &[u8]) -> Result<Self> {
-        Ok(bincode::deserialize(&msg).context("deserializing file meta")?)
     }
 }
